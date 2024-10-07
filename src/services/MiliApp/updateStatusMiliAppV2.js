274e63c763ec207d02c489391fd1cf7e76390616
Leonardo Lyra <lyraleo@gmail.com>
2024-10-07 10:25:41 -0300

    adicionando as funções para atualizar o rastreamento

---
+++ src/services/MiliApp/updateStatusMiliAppV2.js
@@ -0,0 +1,19 @@
+function updateStatusMiliAppV2(raw) {
+    const url = `https://api.fmiligrama.com/vendas/rastreamento/v2`;
+
+    let myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(raw)
+    };
+
+    return fetch(url, requestOptions)
+        .then(response => response.text())
+        .catch(error => console.log('error', error));
+}
+
+export default updateStatusMiliAppV2;
